d7307a3d54a91feb5c74a937805be0a585b6e9b8
Divine <ughedivine@gmail.com>
2024-09-07 09:05:33 -0700

    Prompt

---
+++ prompt.js
@@ -0,0 +1,16 @@
+// let character = prompt("What is ur name ");
+// alert("My name is " + character);
+
+// let character = prompt("What is ur name ");
+//  alert("My name is " + character + " " + character.length );
+
+// let character = prompt("What is ur name ");
+// nameCharacter = character.length;
+//  alert("My name is " + character + " with " + character.length + " character" + " remain " + (200 - nameCharacter));
+
+//  let character = "Dominion";
+//  alert(character.slice(0,3));
+let character = prompt("What is ur name ");
+alert(character.slice(3, 6));
+
+
